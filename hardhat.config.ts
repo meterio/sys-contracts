57970c9e27d33e51755e51f9a17a31b7775b25ea
Simon.Z <zhanghan.simon@gmail.com>
2022-09-27 16:07:41 -0400

    upgrade hardhat for easier verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import "hardhat-typechain";
 import "@nomiclabs/hardhat-ethers";
 import { task } from "hardhat/config";
 import { deployContract, compileSetting, getContract } from "./scripts/helper";
-import {MeterGovERC20Permit} from './typechain'
+import { MeterGovERC20Permit } from "./typechain";
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -13,23 +13,20 @@ export default {
       url: `https://rpctest.meter.io`,
       chainId: 83,
       gasPrice: 500000000000,
-      accounts: [
-        process.env.TESTNET_CONTRACT_ADMIN_PRIVKEY
-      ],
+      accounts: [process.env.CONTRACT_ADMIN_PRIVKEY],
     },
     metermain: {
       url: `https://rpc.meter.io`,
       chainId: 82,
       gasPrice: 500000000000,
-      accounts: [process.env.MAINNET_CONTRACT_ADMIN_PRIVKEY],
+      accounts: [process.env.CONTRACT_ADMIN_PRIVKEY],
     },
     meterstage: {
       url: `http://127.0.0.1:8545`,
       chainId: 82,
       gasPrice: 500000000000,
-      accounts: [process.env.MAINNET_CONTRACT_ADMIN_PRIVKEY],
+      accounts: [process.env.CONTRACT_ADMIN_PRIVKEY],
     },
-
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_APIKEY,
