29708023f0b1842d88c6267601dee4a87701424f
Simon.Z <zhanghan.simon@gmail.com>
2023-02-17 15:18:49 -0800

    add SampleStakingPool contract for testing

--- contracts/SampleStakingPool.sol
+++ contracts/SampleStakingPool.sol
@@ -7,23 +7,33 @@ pragma solidity ^0.8.0;
  * Sample code for Staking Pool on Meter depends on ScriptEngine
  */
 import "./ScriptEngine.sol";
+import "./MeterGovERC20Permit.sol";
 
 contract SampleStakingPool{
     ScriptEngine scriptEngine = ScriptEngine(0x63726970742d656E67696E652D61646472657373);
+    MeterGovERC20Permit MTRG = MeterGovERC20Permit(0x228ebBeE999c6a7ad74A6130E81b12f9Fe237Ba3);
+    bytes32 public poolBucketID;
 
-    function bucketOpen(address candidate, uint256 amount) public returns (bytes32){
-        return scriptEngine.bucketOpen(candidate, amount);
+    function init(address candidate, uint256 amount) public returns (bytes32){
+        require(poolBucketID==bytes32(0), "pool already initialized");
+        MTRG.transferFrom(msg.sender, address(this), amount);
+        poolBucketID = scriptEngine.bucketOpen(candidate, amount);
+        return poolBucketID;
     }
 
-    function bucketDeposit(bytes32 bucketID, uint256 amount) public {
-        return scriptEngine.bucketDeposit(bucketID, amount);
+    function deposit(uint256 amount) public {
+        require(poolBucketID!=bytes32(0), "pool is not initialized");
+        MTRG.transferFrom(msg.sender, address(this), amount);
+        return scriptEngine.bucketDeposit(poolBucketID, amount);
     }
 
-    function bucketWithdraw(bytes32 bucketID, uint256 amount, address recipient) public returns (bytes32 ){
-        return scriptEngine.bucketWithdraw(bucketID, amount, recipient);
+    function withdraw(uint256 amount, address recipient) public returns (bytes32 ){
+        require(poolBucketID!=bytes32(0), "pool is not initialized");
+        return scriptEngine.bucketWithdraw(poolBucketID, amount, recipient);
     }
 
-    function bucketClose(bytes32 bucketID) public {
-        return scriptEngine.bucketClose(bucketID);
+    function destroy() public {
+        require(poolBucketID!=bytes32(0), "pool is not initialized");
+        return scriptEngine.bucketClose(poolBucketID);
     }
 }
